a2f844472c7a39925cec1e845d81be0017ace0b6
nricoz <149686602+nricoz@users.noreply.github.com>
2023-11-10 20:58:43 -0500

    Added comments and put it inside startup function

--- main.c
+++ main.c
@@ -9,27 +9,27 @@ MeDCMotor motor2(M2); // Left
 void setup() {
   // put your setup code here, to run once:
   pinMode(A7, INPUT);
-  while(analogRead(A7) !=0);
+  while(analogRead(A7) !=0); //only starts when button is pressed.
   
   led.setpin(13);
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
   led.setColorAt(0,255,0,0);
   led.setColorAt(1,255,0,0);
   led.show();
   delay(2500);
-  moveRobot(-100, 100, 2400);
-  moveRobot(-100, 0, 1150);
-  moveRobot(-100, 100, 1920); //2400 is around 18 inches (Found through several tests). This means that 133 milliseconds is enough to move the robot 1 inch at a speed of 100. Changed because turn moved the robot. First test: 1862. Second Test: 1995. 2228
-  moveRobot(0, 100, 1150); //Test 90 Degrees: 603 at 100. Second test: 602 at 100. Third test: 601 at 100. Fourth test: 599 at 100. Problem was: Wheels were not well adjusted. 90 degrees is around 599 milliseconds. Tried making one wheel turn. Have to switch delay value. 1st test: 599, 2nd: 1000. Turning on the wrong side. Need to make left positive. 3rd test:1198. Wheel went backwards, need to change to move the other wheel. 90 degrees is turning at 100 speed with the right motor, with a delay of 1198.
-  moveRobot(-100, 100, 1995);
-  delay(3000);
-  moveRobot(-100,100,1330);
-  moveRobot(0,100,1150);
-  moveRobot(-100,100,1197);
-  delay(10000);
+  moveRobot(-100, 100, 2400); //forward
+  moveRobot(-100, 0, 1150); //turn right 90 degrees
+  moveRobot(-100, 100, 1920); //forward
+  moveRobot(0, 100, 1150); //turn left 90 degrees
+  moveRobot(-100, 100, 1995); //forward
+  delay(3000); //delay
+  moveRobot(-100,100,1330); //forward
+  moveRobot(0,100,1150); //turn left 90 degrees
+  moveRobot(-100,100,1197); //go forward
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
 }
 
 void moveRobot(int leftMotor, int rightMotor, int delayTime){
